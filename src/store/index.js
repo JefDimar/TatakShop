06099e7897cd31774191838488245da59599202f
jefDimar <jefdimar@gmail.com>
2021-02-25 06:22:48 +0700

    done fetching products

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from '../api/axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    products: []
   },
   mutations: {
+    fetchProducts (state, data) {
+      state.products = data
+    }
   },
   actions: {
+    FETCH_PRODUCTS (context) {
+      axios.get('/products')
+        .then(({ data }) => {
+          console.log(data)
+          context.commit('fetchProducts', data)
+        })
+        .catch(({ response }) => {
+          console.log(response)
+        })
+    }
   },
   modules: {
   }
